d1aba709e944ca96a7b09de8bb6701f99c24a6eb
NavCGaur <vertigo1112@gmail.com>
2026-05-16 11:45:08 +0530

    SEO: Fix critical redirect loop for sitemap.xml and robots.txt

--- next.config.ts
+++ next.config.ts
@@ -9,17 +9,6 @@ const nextConfig: NextConfig = {
         destination: '/',
         permanent: true,
       },
-      // Force sitemap and robots to non-www if hit via www
-      {
-        source: '/sitemap.xml',
-        destination: 'https://naveengaur.com/sitemap.xml',
-        permanent: true,
-      },
-      {
-        source: '/robots.txt',
-        destination: 'https://naveengaur.com/robots.txt',
-        permanent: true,
-      },
     ]
   },
 };
